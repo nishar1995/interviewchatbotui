c4f481037886900f57c892fd57d3ce994eb34f12
nishar1995 <nisha.rathor@aiinfox.com>
2024-07-31 11:56:38 +0530

    add_loader_disable_button

--- client/src/app/modules/admin/pages/start-interview/index.tsx
+++ client/src/app/modules/admin/pages/start-interview/index.tsx
@@ -78,7 +78,7 @@ export default function StartInterviewDashboard({ id }: any) {
 
             }
             else {
-                utterance = new SpeechSynthesisUtterance('Okay, I will repeat the question. ' + question);
+                // utterance = new SpeechSynthesisUtterance('Okay, I will repeat the question. ' + question);
 
             }
         } else {
@@ -153,14 +153,14 @@ export default function StartInterviewDashboard({ id }: any) {
 
         recognitionRef.current.start();
 
-        silenceDetectionTimeoutRef.current = setTimeout(() => {
-            if (repeatRef.current === 0) {
-                repeatRef.current++;
-                speakQuestion(question);
-            } else {
-                handleAnswer('Candidate did not respond in time or failed to answer.');
-            }
-        }, 5000); // Adjust the timeout duration to suit your needs
+        // silenceDetectionTimeoutRef.current = setTimeout(() => {
+        //     if (repeatRef.current === 0) {
+        //         repeatRef.current++;
+        //         speakQuestion(question);
+        //     } else {
+        //         handleAnswer('Candidate did not respond in time or failed to answer.');
+        //     }
+        // }, 5000); // Adjust the timeout duration to suit your needs
     };
 
     const handleAnswer = (answer: any) => {
@@ -257,6 +257,9 @@ export default function StartInterviewDashboard({ id }: any) {
         if (recognitionRef.current) {
             recognitionRef.current.stop();
         }
+        if (!finalTranscript.trim()) {
+            handleAnswer('');
+        }
         setShowStopButton(false);
         setShowNextButton(true);
         setShowAnswerSection(true);
@@ -273,102 +276,101 @@ export default function StartInterviewDashboard({ id }: any) {
     }
 
     return (
-        <div>
-            {isLoading ? (
-                <div className="flex items-center justify-center h-screen">
+        <div className="relative">
+            {isLoading && (
+                <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50">
                     <Loader variant="spinner" size="lg" />
-                    <p>Please wait Prepare the Questions...</p>
+                    <p>Please wait, preparing the questions...</p>
                 </div>
-            ) : (
-                <>
-                    <div className={`${isLoading ? 'opacity-20' : ''}`}>
-                        <div>
-                            <div className="flex justify-between items-center mb-4">
-                                <h1 className="text-2xl font-bold">Interview Dashboard</h1>
-                                <div className="space-x-2">
-                                    {showStartButton && (
-                                        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={onStart}>Start</button>
-                                    )}
-                                    {!showStartButton && (
-                                        <>
-                                            <button
-                                                className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ${!showSpeakButton ? 'opacity-50 cursor-not-allowed' : ''}`}
-                                                onClick={onSpeak}
-                                                disabled={!showSpeakButton}
-                                            >
-                                                Speak
-                                            </button>
-                                            <button
-                                                className={`bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ${!showStopButton ? 'opacity-50 cursor-not-allowed' : ''}`}
-                                                onClick={onStop}
-                                                disabled={!showStopButton}
-                                            >
-                                                Stop
-                                            </button>
-                                            <button
-                                                className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${!showNextButton ? 'opacity-50 cursor-not-allowed' : ''}`}
-                                                onClick={onNext}
-                                                disabled={!showNextButton}
-                                            >
-                                                Next
-                                            </button>
-                                        </>
-                                    )}
-                                </div>
-                            </div>
-
-                            <div className="card-row">
-                                <div className={`interviewee-card ${isInterviewerSpeaking ? 'green-border' : ''}`}>
-                                    <h3 className="text-gray-700">Interviewer</h3>
-                                    <p>{questions[currentQuestionIndex]?.question}</p>
-                                </div>
-                                <div className={`interviewer-card ${isCandidateSpeaking ? 'green-border' : ''}`}>
-                                    <h3 className="text-gray-700">Candidate</h3>
-                                    <p>Candidate Speak......</p>
-                                    <p>{finalTranscript}</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            {showAnswerSection && (
-                                <section>
-                                    <div id="recording-indicator" style={{ display: 'none' }}>
-                                        <p>Recording...</p>
-                                    </div>
-                                    {showAnswerList.length > 0 ? (
-                                        <div className="table-wrapper">
-                                            <h3>Interview Question/Answer</h3>
-                                            <div className="table-container">
-                                                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                                        <tr>
-                                                            <th scope="col" className="px-6 py-3">Sr.no</th>
-                                                            <th scope="col" className="px-6 py-3">Questions</th>
-                                                            <th scope="col" className="px-6 py-3">Answer</th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                        {showAnswerList.map((item: any, index) => (
-                                                            <tr key={index}>
-                                                                <td className="px-6 py-3">{index + 1}</td>
-                                                                <td className="px-6 py-3">{item.question}</td>
-                                                                <td className="px-6 py-3">{item.answer}</td>
-                                                            </tr>
-                                                        ))}
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </div>
-                                    ) : (
-                                        <p>No answers yet</p>
-                                    )}
-                                </section>
+            )}
+            <div className={`${isLoading ? 'opacity-80' : 'opacity-100'} transition-opacity duration-500`}>
+                <div className="relative z-10">
+                    <div className="flex justify-between items-center mb-4">
+                        <h1 className="text-2xl font-bold">Interview Dashboard</h1>
+                        <div className="space-x-2">
+                            {showStartButton && (
+                                <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={onStart}>Start</button>
+                            )}
+                            {!showStartButton && (
+                                <>
+                                    <button
+                                        className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ${!showSpeakButton ? 'opacity-50 cursor-not-allowed' : ''}`}
+                                        onClick={onSpeak}
+                                        disabled={!showSpeakButton}
+                                    >
+                                        Speak
+                                    </button>
+                                    <button
+                                        className={`bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ${!showStopButton ? 'opacity-50 cursor-not-allowed' : ''}`}
+                                        onClick={onStop}
+                                        disabled={!showStopButton}
+                                    >
+                                        Stop
+                                    </button>
+                                    <button
+                                        className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${!showNextButton ? 'opacity-50 cursor-not-allowed' : ''}`}
+                                        onClick={onNext}
+                                        disabled={!showNextButton}
+                                    >
+                                        Next
+                                    </button>
+                                </>
                             )}
                         </div>
                     </div>
-                </>
-            )}
+    
+                    <div className="card-row">
+                        <div className={`interviewee-card ${isInterviewerSpeaking ? 'green-border' : ''}`}>
+                            <h3 className="text-gray-700">Interviewer</h3>
+                            <p>{questions[currentQuestionIndex]?.question}</p>
+                        </div>
+                        <div className={`interviewer-card ${isCandidateSpeaking ? 'green-border' : ''}`}>
+                            <h3 className="text-gray-700">Candidate</h3>
+                            <p>Candidate Speak...</p>
+                            <p>{finalTranscript}</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    {showAnswerSection && (
+                        <section>
+                            <div id="recording-indicator" style={{ display: 'none' }}>
+                                <p>Recording...</p>
+                            </div>
+                            {showAnswerList.length > 0 ? (
+                                <div className="table-wrapper">
+                                    <h3>Interview Question/Answer</h3>
+                                    <div className="table-container">
+                                        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                                <tr>
+                                                    <th scope="col" className="px-6 py-3">Sr.no</th>
+                                                    <th scope="col" className="px-6 py-3">Questions</th>
+                                                    <th scope="col" className="px-6 py-3">Answer</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {showAnswerList.map((item: any, index) => (
+                                                    <tr key={index}>
+                                                        <td className="px-6 py-3">{index + 1}</td>
+                                                        <td className="px-6 py-3">{item.question}</td>
+                                                        <td className="px-6 py-3">{item.answer}</td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            ) : (
+                                <p>No answers yet</p>
+                            )}
+                        </section>
+                    )}
+                </div>
+            </div>
         </div>
     );
+    
+    
 
 }
