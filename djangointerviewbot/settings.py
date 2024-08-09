092a1d443e454bc4b26320bf1f097a3dc9df0b6f
AiINFOX <aiinfox@AiINFOXs-MacBook-Pro.local>
2024-08-09 09:14:51 +0530

    yml and db change

--- djangointerviewbot/settings.py
+++ djangointerviewbot/settings.py
@@ -139,14 +139,14 @@ ASGI_APPLICATION = 'Flask_Bot1 2.asgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'interviewbot',
-        'USER': 'chatappdev',
-        'PASSWORD': 'IamChatAppDJapp001',
-        'HOST': 'intapp.learninginbits.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'interviewbot',
+        # 'USER': 'chatappdev',
+        # 'PASSWORD': 'IamChatAppDJapp001',
+        # 'HOST': 'intapp.learninginbits.com',
+        # 'PORT': '3306',
     }
 }
 
